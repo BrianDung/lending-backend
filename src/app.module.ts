f42f38aa3bd1aafee49e06f6e38bfaf84de20cf7
simon-fast-startup <simon@faststartup.io>
2022-11-07 11:38:01 +0700

    feat: update migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,32 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmAsyncConfig } from './config/typeorm.config';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : null,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
-      migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-      migrationsRun: false,
-      // cli: {
-      //   entitiesDir: 'src',
-      //   migrationsDir: 'migrations',
-      // },
-      // Timezone configured on the MySQL server.
-      // This is used to typecast server date/time values to JavaScript Date object and vice versa.
-      timezone: 'Z',
-      synchronize: false,
-      // debug: process.env.NODE_ENV === 'development',
-    }),
-    UsersModule,
-  ],
+  imports: [TypeOrmModule.forRootAsync(typeOrmAsyncConfig), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
